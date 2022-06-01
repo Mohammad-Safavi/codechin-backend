7cb7ad54651cf03281d0e6f0dabd368a592652ad
msfv1382 <mohammadsafavi999@yahoo.com>
2022-06-02 00:33:55 +0430

    add filters and fixed bug

--- product/models.py
+++ product/models.py
@@ -6,28 +6,38 @@ class Category(models.Model):
     title = models.CharField(max_length=200)
     icon = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.title
 
 class SubCategory(models.Model):
     title = models.CharField(max_length=200)
     icon = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
 
 class SubSubCategory(models.Model):
     title = models.CharField(max_length=200)
     icon = models.TextField(null=True, blank=True)
     sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
 
 class Picture(models.Model):
     name = models.CharField(max_length=100)
     path = models.CharField(max_length=600)
 
+    def __str__(self):
+        return self.name
 
 class Property(models.Model):
     title = models.CharField(max_length=200)
     value = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.title + ' => ' + self.value
 
 class Option(models.Model):
 
@@ -42,12 +52,15 @@ class Option(models.Model):
     title = models.CharField(max_length=200)
     type = models.SmallIntegerField(choices=CHOICES_OPTION, default=OPTION_TYPE_COLOR)
 
+    def __str__(self):
+        return self.title + ' | ' + str(self.type)
 
 class Discount(models.Model):
     title = models.CharField(max_length=200)
     percent = models.IntegerField(validators=[MinValueValidator(1),
                                               MaxValueValidator(100)])
-
+    def __str__(self):
+        return self.title
 
 class Product(models.Model):
 
@@ -59,12 +72,16 @@ class Product(models.Model):
     )
 
     name = models.CharField(max_length=200)
+    english_name = models.CharField(max_length=200,null=True)
     price = models.IntegerField(validators=[MinValueValidator(0)])
     discount = models.ForeignKey(Discount,  null=True,blank=True,on_delete=models.CASCADE)
     pictures = models.ManyToManyField(Picture)
     properties = models.ManyToManyField(Property)
-    options = models.ManyToManyField(Option)
+    options = models.ManyToManyField(Option, null=True)
     category = models.ManyToManyField(SubSubCategory)
     about = models.TextField(blank=True)
     status = models.SmallIntegerField(choices=CHOICES_STATUS, default=STATUS_AVAILABLE)
     active = models.BooleanField(default=1)
+
+    def __str__(self):
+        return self.name
--- product/views.py
+++ product/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.db.models import Q
 from rest_framework import viewsets
 from .models import *
 from .serializers import *
 
+
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+    def get_queryset(self):
+        queryset = Product.objects
+        stdi = self.request.query_params.get('is_discount', None)
+        num = self.request.query_params.get('page_size')
+        if stdi == 'true':
+            queryset = Product.objects.filter(~Q(discount=None))
+        return queryset.all()[:int(num)]
